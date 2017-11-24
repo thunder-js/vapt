6b429f12b129ad5e834c0622eaf9271715b861c1
Rafael Ribeiro <rafael.correia.poli@gmail.com>
2017-11-24 00:12:37 -0200

    Give vapt :)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
-import * as mySubmodule from './my-submodule'
+const isPromise = thing => thing instanceof Promise
 
-export {
-  mySubmodule
+const buildStartAction = type => ({
+  type,
+})
+
+const buildSuccessAction = (type, data) => ({
+  type,
+  payload: data,
+})
+
+const buildErrorAction = (type, error) => ({
+  type,
+  error,
+})
+
+const defaultConfig = {
+  startTypeGetter: action => `${action.type}_START`,
+  successTypeGetter: action => `${action.type}_SUCCESS`,
+  errorTypeGetter: action => `${action.type}_ERROR`,
+}
+export const middlewareCreator = userConfig => store => next => (action) => {
+  const { payload } = action
+  const config = {
+    ...defaultConfig,
+    userConfig,
+  }
+
+  if (isPromise(payload)) {
+    const type = config.startTypeGetter(action)
+    store.dispatch(buildStartAction(type, action))
+    return payload.then((data) => {
+      const successType = config.successTypeGetter(action)
+      store.dispatch(buildSuccessAction(successType, data))
+      return data
+    })
+      .catch((error) => {
+        const errorType = config.errorTypeGetter(action)
+        store.dispatch(buildErrorAction(errorType, error))
+        throw error
+      })
+  }
+  next(action)
 }
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,91 @@
-import { mySubmodule } from './'
+import { createStore, applyMiddleware } from 'redux'
+import { middlewareCreator } from './index'
 
-it('can use functions inside module', () => {
-	expect(mySubmodule.sum(1, 2)).toEqual(3)
-	expect(mySubmodule.multiply(2, 3)).toEqual(6)
+const rootReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'FETCH_START':
+      return {
+        ...state,
+        message: 'start',
+      }
+    case 'FETCH_SUCCESS':
+      return {
+        ...state,
+        message: 'success',
+        data: action.payload,
+      }
+    case 'FETCH_ERROR':
+      return {
+        ...state,
+        message: 'error',
+        error: action.error,
+      }
+    default:
+      return state
+  }
+}
+
+
+const FETCH = 'FETCH'
+
+const promise = () => new Promise((resolve) => {
+  resolve('rafael')
+})
+
+const promiseError = () => new Promise(() => {
+  throw new Error('my-error')
+})
+
+const fetch = () => ({
+  type: FETCH,
+  payload: promise(),
+})
+const fetchError = () => ({
+  type: FETCH,
+  payload: promiseError(),
+})
+
+const config = {
+  startTypeGetter: action => `${action.type}_START`,
+  successTypeGetter: action => `${action.type}_SUCCESS`,
+  errorTypeGetter: action => `${action.type}_ERROR`,
+}
+describe('middleware', () => {
+  it('works for resolving promises', async () => {
+    const store = createStore(rootReducer, applyMiddleware(middlewareCreator(config)))
+    store.dispatch(fetch())
+      .then((data) => {
+        expect(data).toEqual('rafael')
+        const state = store.getState()
+        expect(state).toEqual({
+          message: 'success',
+          data: 'rafael',
+        })
+      })
+    const state = store.getState()
+
+    expect(state).toEqual({
+      message: 'start',
+    })
+  })
+
+  it('works for failing promises', async () => {
+    const store = createStore(rootReducer, applyMiddleware(middlewareCreator(config)))
+    store.dispatch(fetchError())
+      .then(() => {
+        // if this code gets executed, something is wrong :(
+        expect(true).toEqual(false)
+      })
+      .catch((returnedError) => {
+        expect(returnedError.message).toBe('my-error')
+        const state = store.getState()
+        const { error, message } = state
+        expect(message).toBe('error')
+        expect(error.message).toBe('my-error')
+      })
+    const state = store.getState()
+    expect(state).toEqual({
+      message: 'start',
+    })
+  })
 })
